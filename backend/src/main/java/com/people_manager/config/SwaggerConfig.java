e986f2f06af147ca85e1a16b78cc39f0b1280baf
davif <davifariagoncalves@gmail.com>
2025-12-10 00:05:36 -0300

    feat(backend): implementa configurações de segurança e swagger

---
+++ backend/src/main/java/com/people_manager/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.people_manager.config;
+
+import com.people_manager.base.config.ApiSwaggerConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig extends ApiSwaggerConfig {
+}
